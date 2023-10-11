4193d903b791030fd08734093101643e7822f4cf
Flame-Neon-tetra <2101936@uad.ac.uk>
2023-10-11 12:22:42 +0100

    new c#

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("new c# stuff");
+            int nume = 0;
+
+            nume =  nume + 100 / 2 * 3;
+
+            Console.WriteLine(nume);
+        }
+    }
+}
